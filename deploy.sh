bedc76adad82929c9e86c42ede193b85f005a82b
Jean-Denis Vauguet <jd@vauguet.fr>
2011-08-07 07:14:07 +0200

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# assuming a git checkout -b publish was performed from master, with some config edits commited on publish
+git branch -d publish
+git checkout -b publish
+git checkout -- public
+git rebase master
+jekyll
+git add -A .
+git commit -m "static content update"
+
+git checkout gh-pages
+rm -rf *
+for file in `git ls-tree publish public -r --name-only`; do
+  `git checkout publish $file` && cp -r public/* . && rm -rf public && touch .nojekyll;
+done
+git add -A .
+git ci -m "content update"
+git push origin gh-pages
+git checkout master
